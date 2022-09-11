b0caf9c2555eba837d6b11ebbb08ec0975e821ee
roshan maskey <roshanmaskey@gmail.com>
2022-09-11 20:55:51 +0000

    Updated setup script to work with ver 0.0.4

--- script/setup.sh
+++ script/setup.sh
@@ -5,20 +5,20 @@
 set -e
 SCRIPTNAME=`basename $0`
 
-CE_VER=0.0.3
-CE_PKG=container-explorer-0.0.3-20220612.tar.gz
+CE_VER=0.0.4
+CE_PKG=container-explorer.tar.gz
 
 CE_DIR=/opt/container-explorer
 CE_BIN=${CE_DIR}/bin/ce
 CE_SUPPORT=${CE_DIR}/etc/supportcontainer.yaml
 
 TMP_DIR=$(mktemp -d -t ce-XXXXXXXXX)
-CE_TMP_DIR=${TMP_DIR}/container-explorer-0.0.3-20220612
+CE_TMP_DIR=${TMP_DIR}/container-explorer
 
 download_release() {
   echo "[+] Downloading container-explorer ${CE_PKG}"
   wget -P ${TMP_DIR} https://github.com/google/container-explorer/releases/download/${CE_VER}/${CE_PKG} > /dev/null 2>&1
-  tar -zxf ${TMP_DIR}/container-explorer-0.0.3-20220612.tar.gz -C ${TMP_DIR} > /dev/null 2>&1
+  tar -zxf ${TMP_DIR}/container-explorer.tar.gz -C ${TMP_DIR} > /dev/null 2>&1
 }
 
 
@@ -38,7 +38,7 @@ install_release() {
   fi
 
   # Copy new binary and support file
-  cp -f ${CE_TMP_DIR}/container-explorer ${CE_BIN}
+  cp -f ${CE_TMP_DIR}/ce ${CE_BIN}
   cp -f ${CE_TMP_DIR}/supportcontainer.yaml ${CE_SUPPORT}
 }
 
